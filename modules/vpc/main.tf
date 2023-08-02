4e90470cd96be87ce7e175f48eb85935b1217f02
alvo254 <alvinkhamron@gmail.com>
2023-08-03 00:00:38 +0300

    sg and rt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,6 +12,9 @@ resource "aws_vpc" "vpc" {
 
 data "aws_availability_zones" "available_zones" {}
 
+
+
+
 resource "aws_subnet" "public_subnet1" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = var.public_subnet
@@ -36,5 +39,41 @@ resource "aws_subnet" "public_subnet_az1" {
         Name = "${var.project}-${var.environment}-public-az1"
     }
 }
-  
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+
+    route {
+        ipv6_cidr_block = "::/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+
+    tags = {
+        Name = "${var.project}-${var.environment}-public-rt"
+    }
+}
+
+
+resource "aws_route_table_association" "public_subnet_az1_rt_association" {
+    subnet_id      = aws_subnet.public_subnet1.id
+    route_table_id = aws_route_table.public_route_table.id
+}
+
+# associate public subnet az2 to "public route table"
+resource "aws_route_table_association" "public_subnet_2_rt_association" {
+    subnet_id      = aws_subnet.public_subnet_az1.id
+    route_table_id = aws_route_table.public_route_table.id
+}
